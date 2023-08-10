80805e35d9431bc8154c14ee027925915cb842b4
Ars Vladimirov <grokhael@gmail.com>
2023-08-10 20:40:44 +0600

    HIDE_DATASET = False

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "CHASE DB1"
 PROJECT_NAME_FULL: str = "CHASE DB1: Retinal Vessel Reference Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
